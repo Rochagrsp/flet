1ce27926cbf3ffec17fa776aca168789e991beba
Rochagrsp <gabrielpereirarocha1710@gmail.com>
2026-05-21 21:06:52 -0300

    Flet

--- calculadora.py
+++ calculadora.py
@@ -10,14 +10,46 @@ def main(page: ft.Page):
 
     resultado=ft.Text("Resultado:")
 
-    def calcular():
+    def calcular(e):
        n1= float(numero1.value)
        n2= float(numero2.value)
+    
 
-       
+       operacao = operacoes.value
 
+       if operacao == '+':
+           conta= n1+n2
+       elif operacao=="-":
+           conta=n1-n2
+       elif operacao=="*":
+           conta=n1*n2
+       elif operacao=="/":
+           conta=n1/n2  
 
-    page.add(numero1,numero2,resultado)
+       resultado.value=f"Resultado:{conta}" 
+       page.update()
+
+           
+
+    operacoes = ft.Dropdown(
+            label='Escolha a operação',
+            options=[
+                ft.dropdown.Option("+"),
+                ft.dropdown.Option("-"),
+                ft.dropdown.Option("*"),
+                ft.dropdown.Option("/"),
+           ]
+       )
+    botao=ft.ElevatedButton(
+        "Calcular",
+        on_click=calcular
+    )
+
+    page.add(numero1,
+             numero2,
+             resultado,
+             operacoes,
+             botao)
 
 
 ft.app(target=main)
